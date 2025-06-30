55bad09eda371ab54c2c050e49da9191d1842ed9
Konrad Wiśniewski <konrad.wisniewski87@gmail.com>
2025-06-30 22:37:57 +0200

    Added mqtt controller

---
+++ TestMQTT/Controllers/MqttController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestMQTT.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class MqttTestController : ControllerBase
+{
+    private readonly MqttService _mqtt;
+
+    public MqttTestController(MqttService mqtt) => _mqtt = mqtt;
+
+    [HttpPost("open")]
+    public async Task<IActionResult> Open() =>
+        Ok(await PublishAndLog("fox/str1s2/192.168.0.101/command", "open"));
+
+    [HttpPost("close")]
+    public async Task<IActionResult> Close() =>
+        Ok(await PublishAndLog("fox/str1s2/192.168.0.101/command", "close"));
+
+    private async Task<string> PublishAndLog(string topic, string msg)
+    {
+        await _mqtt.PublishAsync(topic, msg);
+        return $"Sent '{msg}' to '{topic}'";
+    }
+}
